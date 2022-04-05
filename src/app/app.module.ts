09eda79968822246bb4a1e3aa13fa82a15085ff8
Sangeetha <>
2022-04-05 11:26:01 +0530

    added file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgxNavbarModule } from 'ngx-bootstrap-navbar';
+import { NgxBootstrapIconsModule, allIcons } from 'ngx-bootstrap-icons';
+import { search, bell, wrench, personCircle } from 'ngx-bootstrap-icons';
+
+const icons = {
+  search,
+  bell,
+  wrench,
+  personCircle
+
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
+
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    NgbCollapseModule,
+    NgxNavbarModule,
+    NgxBootstrapIconsModule.pick(icons, {
+      width: '1.1em',
+      height: '1.1em',
+
+    })
+
   ],
   providers: [],
   bootstrap: [AppComponent]
